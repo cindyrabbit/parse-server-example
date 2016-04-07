e7ceb0432559b6f94f18ef308d90fd4c137f66fe
Chris Li <chris.li.2013@hotmail.com>
2016-04-07 15:39:20 -0700

    Change mongodb target to hit azure lab

--- startDev.sh
+++ startDev.sh
@@ -1,6 +1,7 @@
 export APP_ID="myAppId"
 export MASTER_KEY="myMasterKey" 
 export PORT=1338 
-export DATABASE_URI="mongodb://chris:870807@localhost:27017/dev"
+#export DATABASE_URI="mongodb://chris:870807@localhost:27017/dev"
+export DATABASE_URI="mongodb://chris:870807@mongodb.westus.cloudapp.azure.com:27017/dev"
 export JAVASCRIPT_KEY="myJavascriptKey"
 npm start
