bd2628a0c701cb47b48d4d91f6069e96f4aafad2
Chris Li <chris.li.2046@gmail.com>
2016-04-07 12:28:55 -0700

    Added JAVACRIPT_KEY parameter

--- startDev.sh
+++ startDev.sh
@@ -2,4 +2,5 @@ export APP_ID="myAppId"
 export MASTER_KEY="myMasterKey" 
 export PORT=1338 
 export DATABASE_URI="mongodb://chris:870807@localhost:27017/dev"
+export JAVASCRIPT_KEY="myJavascriptKey"
 npm start
